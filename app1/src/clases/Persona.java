388a5dd100febb75167249fe7922bbd7c46c87fb
FernandoLepore <juliofernandolepore@gmail.com>
2022-10-23 01:51:24 -0300

    creacion de archivo main y archivo package para creacion de clases y objetos(instancias)

---
+++ app1/src/clases/Persona.java
@@ -0,0 +1,16 @@
+
+package clases;
+
+public class Persona {
+    //atributos de esta clase
+    String nombre;
+    String apellido;
+    int edad;    
+    //metodos de la clase
+    public void mostrarInformacion(){
+    System.out.println("nombre " + nombre);
+    System.out.println("apellido" + apellido);
+    System.out.println("edad: " + edad);
+    
+    }
+}
